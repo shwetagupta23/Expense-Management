7076f78bd0748bec0770ebb4dc4bd2cb01e60549
shwetagupta23 <Shwetagupta23@navgurukul.org>
2024-10-08 14:39:07 +0530

    first commit

---
+++ expensemanager/expensemanager/doctype/expanse_type/expanse_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, ShW and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Expanse Type", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ expensemanager/expensemanager/doctype/expanse_type/test_expanse_type.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, ShW and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestExpanseType(FrappeTestCase):
+	pass
